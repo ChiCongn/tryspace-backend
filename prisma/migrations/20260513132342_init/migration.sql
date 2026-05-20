54f7181adc5d39f506e184c3f224196bf8e9e3c2
chicongn <chicongvn1205@gmail.com>
2026-05-20 21:11:54 +0700

    chore(db): add index cleanup migration

---
+++ prisma/migrations/20260513132342_init/migration.sql
@@ -0,0 +1,56 @@
+-- DropIndex
+DROP INDEX "idx_cartitem_userid";
+
+-- DropIndex
+DROP INDEX "idx_design_userid";
+
+-- DropIndex
+DROP INDEX "idx_order_createdat";
+
+-- DropIndex
+DROP INDEX "idx_order_status";
+
+-- DropIndex
+DROP INDEX "idx_order_userid";
+
+-- DropIndex
+DROP INDEX "idx_orderitem_order";
+
+-- DropIndex
+DROP INDEX "idx_orderitem_product";
+
+-- DropIndex
+DROP INDEX "idx_product_averagerating";
+
+-- DropIndex
+DROP INDEX "idx_product_baseprice";
+
+-- DropIndex
+DROP INDEX "idx_product_category";
+
+-- DropIndex
+DROP INDEX "idx_product_createdat";
+
+-- DropIndex
+DROP INDEX "idx_product_isactive";
+
+-- DropIndex
+DROP INDEX "idx_refreshtoken_expiresat";
+
+-- DropIndex
+DROP INDEX "idx_refreshtoken_userid";
+
+-- DropIndex
+DROP INDEX "idx_review_productid";
+
+-- DropIndex
+DROP INDEX "idx_review_rating";
+
+-- DropIndex
+DROP INDEX "idx_review_status";
+
+-- DropIndex
+DROP INDEX "idx_user_isactive";
+
+-- DropIndex
+DROP INDEX "idx_user_role";
